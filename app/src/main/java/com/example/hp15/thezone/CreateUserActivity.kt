418bd1550b553f5dfec1c06fb648d587d6144fdf
Alex Mulwa <lexmulwa@gmail.com>
2018-10-19 19:05:27 +0300

    create user

---
+++ app/src/main/java/com/example/hp15/thezone/CreateUserActivity.kt
@@ -0,0 +1,28 @@
+package com.example.hp15.thezone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class CreateUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+    }
+
+    fun generateUserAvatar(view: View)
+    {
+
+    }
+
+    fun generateColorclicked(view: View)
+    {
+
+    }
+
+    fun createUserclicked(view: View)
+    {
+
+    }
+}
